4ca1aa538d7ad0ef22283c717b505520e986f62a
Marcel Hoyer <mhoyer@pixelplastic.de>
2017-01-12 16:17:57 +0100

    add text/html support for better response when called in browser

--- roomstatus.js
+++ roomstatus.js
@@ -3,7 +3,17 @@ var rpio = require('rpio');
 rpio.open(12, rpio.INPUT);
 function handleRequest(request, response)
 {
-  response.end(rpio.read(12) == 1 ? "Licht aus" : "Licht an" );
+  response.setHeader('Content-Type', 'text/html');
+  response.end(`
+<!DOCTYPE html>
+<html>
+<body style="overflow: hidden">
+<div style="display: flex; justify-content: center; align-items: center; width: 100vw; height: 100vh;">
+    <h1 style="font-family: monospace; font-size: 48px;">
+        ${rpio.read(12) == 1 ? "Licht aus" : "Licht an"}
+    </h1>
+</div>
+</body>`);
 }
 
 
